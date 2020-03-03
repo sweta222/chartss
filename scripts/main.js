2711c1e503f6f6cdfca673f8d4bc10920b292dd1
sweta222 <swetastuti2@gmail.com>
2020-03-03 19:19:07 +0530

    1st commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// var mySong = document.querySelector('h1');
+// mySong.textContent = 'Hello World!' ;
+
+
+
+//  document.getElementById("coco").innerHTML = function () {
+//      myfunction(h);
+//  };
+//  function myfunction(g) {
+//     const koko = "hello world";
+//      return koko;
+//  }
