0f75089dddbb4460c5b704821ce3dd34a915481d
sweta222 <swetastuti2@gmail.com>
2020-03-05 09:03:15 +0530

    added function for string pallindrome check

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,19 @@
   const mySong = document.querySelector('h1');
-  alert("hello world");
+  //alert("hello world");
   mySong.textContent = 'Hello World!';
+
+  function myFunction() {
+    var revString = "";
+    var input = document.getElementById("mybutton").value;
+    var i = input.length;
+
+    for (var j = i; j >=0; j--){
+         revString = revString  + input.charAt(j);
+    }
+   if(input === revString){
+     alert(input + "is pall");
+   }
+   else{
+     alert(input + "isn't pall");
+   }
+  }
