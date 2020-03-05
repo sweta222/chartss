5eaae61d81fc180b6c9486bf85c4f900e0eead8e
sweta222 <swetastuti2@gmail.com>
2020-03-05 10:22:39 +0530

    added new method for string pallindrome check

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,30 @@
   mySong.textContent = 'Hello World!';
 
   function myFunction() {
-    var revString = "";
-    var input = document.getElementById("mybutton").value;
-    var i = input.length;
+  //method1
+  
+  //   var revString = "";
+  //   var input = document.getElementById("mybutton").value;
+  //   var i = input.length;
 
-    for (var j = i; j >=0; j--){
-         revString = revString  + input.charAt(j);
-    }
-   if(input === revString){
-     alert(input + "is pall");
-   }
-   else{
-     alert(input + "isn't pall");
-   }
+  //   for (var j = i; j >=0; j--){
+  //        revString = revString  + input.charAt(j);
+  //   }
+  //  if(input === revString){
+  //    alert(input + "is pall");
+  //  }
+  //  else{
+  //    alert(input + "isn't pall");
+  //  }
+
+  //method2
+  var input = document.getElementById("mybutton").value;
+  var z = input.toLowerCase().replace(/\W/g, "");
+  var t = z.split("").reverse().join("");
+  if(t === input){
+    alert(input+ "is pallindrome");
+  }
+  else{
+    alert(input+ "isnt pallindrome");
+  }
   }
