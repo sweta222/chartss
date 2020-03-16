5f55339fb6720b7007fff16311fc9c92e782af46
sweta222 <swetastuti2@gmail.com>
2020-03-16 10:19:37 +0530

    added a new method

--- chartjs-sample/scripts/charts.js
+++ chartjs-sample/scripts/charts.js
@@ -1,47 +1,56 @@
+//method1
+// Chart.defaults.global.defaultFontFamily = 'Lato';
+// Chart.defaults.global.defaultFontSize = 27;
+// Chart.defaults.global.defaultFontColor = '#777';
+
+// //bar chart creation
+// let drawBar = document.getElementById("barchart").getContext("2d");
+// let barChart = new Chart(drawBar, {
+//     type: 'bar',
+//     responsive: 'false',
+//     data: {
+//       labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
+//       datasets: [{
+//         label: 'density of planets (kg/m3)',
+//         data: [5427, 5243, 5514, 3933, 1326, 687, 1271, 1638],
+//         backgroundColor: 'orange'
+//       }]
+//     },
+//     options: {
+//       title: {
+//         display: true,
+//         text: 'planets data',
+//         fontSize: 30
+//       }
+//     }
+// });
+
+//method2
 Chart.defaults.global.defaultFontFamily = 'Lato';
 Chart.defaults.global.defaultFontSize = 27;
 Chart.defaults.global.defaultFontColor = '#777';
 
-//bar chart creation
-let drawBar = document.getElementById("barchart").getContext("2d");
-let barChart = new Chart(drawBar, {
-    type: 'bar',
-    responsive: 'false',
-    data: {
-      labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
-      datasets: [{
-        label: 'density of planets (kg/m3)',
-        data: [5427, 5243, 5514, 3933, 1326, 687, 1271, 1638],
-        backgroundColor: 'orange'
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'planets data',
-        fontSize: 30
-      }
-    }
-});
+let data = {
+  "Mercury": 5427,
+  "Venus": 5243,
+  "Earth": 5514,
+  "Mars": 3933,
+  "Jupiter": 1326,
+  "Saturn": 687,
+  "Uranus": 1271,
+  "Neptune": 1638
+}
 
-//line chart creation 
-let drawLine = document.getElementById("linechart").getContext('2d');
-let lineChart = new Chart(drawLine, {
-  type: 'line',
-  responsive: 'false',
+const drawBar = document.getElementById('barchart');
+
+let barChart = new Chart(drawBar, {
+  type: 'bar',
   data: {
-    labels: ["0s", "10s", "20s", "30s", "40s", "50s", "60s"],
-    datasets: [{
-      label: 'car speed (mph)',
-      data: [0, 59, 75, 20, 20, 55, 40],
-      backgroundColor: 'grey'
-    }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'speed data',
-      fontSize: 30
-    }
+     labels: Object.keys(data),
+     datasets: [{
+        label: 'density of planets (kg/m3)',
+        backgroundColor: 'orange',
+        data: Object.values(data)
+     }]
   }
-});
+})
