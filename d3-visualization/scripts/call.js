c77bb08d92cfd8cbf5296aa6c59495f3f901b313
sweta222 <swetastuti2@gmail.com>
2020-03-18 09:18:56 +0530

    added this new file for better user experience in drawing charts

--- d3-visualization/scripts/call.js
+++ d3-visualization/scripts/call.js
@@ -1,1 +1,27 @@
-draw("m");
+draw('.canvas', 
+{"type": "bar",
+
+width:860,
+height:500,
+
+"datasets":[
+    {
+    "labelx": "compost",
+    "labely": 200
+    },
+    {
+    "labelx": "paper and card",
+    "labely": 600
+    },
+    {
+    "labelx": "glass",
+    "labely": 300
+    },
+    {
+    "labelx": "co-mingled material",
+    "labely": 900
+    }
+]
+
+}
+);
