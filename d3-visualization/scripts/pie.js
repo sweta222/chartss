691352286e9b7aef8dbf2228b75b0556b75b8680
sweta222 <swetastuti2@gmail.com>
2020-03-20 21:30:01 +0530

    newly added file for making a pie chart with all visualizations stuffs

---
+++ d3-visualization/scripts/pie.js
@@ -0,0 +1,78 @@
+const drawPie = (
+  parentGroup,
+  piee,
+  arcs,
+  colorx,
+  arcLabel,
+  browsers,
+  svg,
+  radius,
+  svgwidth,
+  legendFill,
+  legendText,
+  pieTextanchor,
+  Heading,
+  pieFontsize,
+  HeadingStyle,
+  HeadingStylex,
+  HeadingStyley,
+  HeadingSize,
+  HeadingColor,
+  pieLegWidth,
+  pieLegHeight,
+  legendTextsize,
+  legYpost,
+  legXpost
+) => {
+  const g = parentGroup
+    .selectAll('arc')
+    .data(piee)
+    .enter()
+    .append('g');
+
+  g.append('path')
+    .attr('d', arcs)
+    .attr('fill', colorx);
+
+  g.append('text')
+    .attr('transform', d => 'translate(' + arcLabel.centroid(d) + ')')
+    .style('font-size', pieFontsize)
+    .text(browsers)
+    .attr('text-anchor', pieTextanchor);
+
+  svg
+    .append('text')
+    .attr('transform', 'translate(400, 200)')
+    .text(Heading)
+    .attr('font-family', HeadingStyle)
+    .attr('x', HeadingStylex)
+    .attr('y', HeadingStyley)
+    .attr('font-size', HeadingSize)
+    .attr('fill', HeadingColor);
+
+  parentGroup.attr('transform', 'translate(' + radius + ',' + radius + ')');
+
+  const legendGrp = svg
+    .selectAll('.legend')
+    .data(piee)
+    .enter()
+    .append('g')
+    .attr(
+      'transform',
+      (d, i) => 'translate(' + (svgwidth - 300) + ',' + (i * 30 + 20) + ')'
+    )
+    .attr('class', 'legend');
+
+  legendGrp
+    .append('rect')
+    .attr('width', pieLegWidth)
+    .attr('height', pieLegHeight)
+    .attr('fill', legendFill);
+
+  legendGrp
+    .append('text')
+    .text(legendText)
+    .style('font-size', legendTextsize)
+    .attr('y', legYpost)
+    .attr('x', legXpost);
+};
