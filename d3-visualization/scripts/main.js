26cdda731ee0c226ef4f72cb601afe8bb01d77c1
sweta222 <swetastuti2@gmail.com>
2020-03-20 21:32:57 +0530

    removed manually inputed margin and added some parametres in the drawbar,drawline along with adding a new function call for pie chart creation

--- d3-visualization/scripts/main.js
+++ d3-visualization/scripts/main.js
@@ -1,8 +1,18 @@
 const draw = (selectorName, data) => {
-
-  const margin = { top: 20, right: 20, bottom: 60, left: 120 };
+  const margin = {
+    top: data.top,
+    right: data.right,
+    bottom: data.bottom,
+    left: data.left
+  };
   const graphWidth = data.width - margin.left - margin.right;
   const graphHeight = data.height - margin.top - margin.bottom;
+  const radius =
+    Math.min(
+      graphWidth + margin.left + margin.right,
+      graphHeight + margin.top + margin.bottom
+    ) / 2;
+
   const svg = d3
     .select(selectorName)
     .append('svg')
@@ -16,28 +26,44 @@ const draw = (selectorName, data) => {
     .attr('height', graphHeight)
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-  const yScale = drawLinearAxis(
-    0,
-    d3.max(data.datasets, d => d.labely),
-    graphHeight,
-    0,
-    graph,
-    data.yTextsize
-  );
-  const xScale = drawBandAxis(
-    data.datasets.map(d => d.labelx),
-    0,
-    graphWidth,
-    graph,
-    graphHeight,
-    data.xTextsize,
-    data.xTransform,
-    data.xTextAnchor,
-    data.innerPadding,
-    data.outerPadding
-  );
+  const colors = d3.scaleOrdinal().range(data.barColor);
+
+  const color = d3.scaleOrdinal().range(data.piecolors);
+
+  const pie = d3.pie()(data.datasets.map(d => d.labely));
+
+  const arc = d3
+    .arc()
+    .outerRadius(radius - 10)
+    .innerRadius(data.innerRadius);
+
+  const labelArc = d3
+    .arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
+
   switch (data.type) {
     case 'bar':
+      const yScale = drawLinearAxis(
+        0,
+        d3.max(data.datasets, d => d.labely),
+        graphHeight,
+        0,
+        graph,
+        data.yTextsize
+      );
+      const xScale = drawBandAxis(
+        data.datasets.map(d => d.labelx),
+        0,
+        graphWidth,
+        graph,
+        graphHeight,
+        data.xTextsize,
+        data.xTransform,
+        data.xTextAnchor,
+        data.innerPadding,
+        data.outerPadding
+      );
       drawBar(
         xScale,
         yScale,
@@ -47,12 +73,75 @@ const draw = (selectorName, data) => {
         'labely',
         graphHeight,
         xScale.bandwidth,
-        data.barColor
+        (d, i) => colors(i),
+        svg,
+        data.Heading,
+        data.HeadingSize,
+        data.LegXpost,
+        data.LegYpost
       );
-    break;
+      break;
     case 'line':
-      drawLine(xScale, yScale, data.datasets, 'labelx', 'labely', graph, data.linestrokecolor, data.lineArea, data.strokeWidth);
+      const yScale2 = drawLinearAxis(
+        0,
+        d3.max(data.datasets, d => d.labely),
+        graphHeight,
+        0,
+        graph,
+        data.yTextsize
+      );
+      const xScale2 = drawBandAxis(
+        data.datasets.map(d => d.labelx),
+        0,
+        graphWidth,
+        graph,
+        graphHeight,
+        data.xTextsize,
+        data.xTransform,
+        data.xTextAnchor,
+        data.innerPadding,
+        data.outerPadding
+      );
+      drawLine(
+        xScale2,
+        yScale2,
+        data.datasets,
+        'labelx',
+        'labely',
+        graph,
+        data.linestrokecolor,
+        data.lineArea,
+        data.strokeWidth
+      );
+      break;
+    case 'pie':
+      drawPie(
+        graph,
+        pie,
+        arc,
+        (d, i) => color(i),
+        labelArc,
+        (d, i) => data.datasets[i].labelx,
+        svg,
+        radius,
+        graphWidth + margin.left + margin.right,
+        (d, i) => color(i),
+        (d, i) => data.datasets[i].labelx,
+        data.xTextAnchor,
+        data.Heading,
+        data.pieFontsize,
+        data.HeadingStyle,
+        data.HeadingStyleXval,
+        data.HeadingStyleYval,
+        data.HeadingSize,
+        data.HeadingColor,
+        data.LegendWidth,
+        data.LegendHeight,
+        data.LegendTextSize,
+        data.LegYpost,
+        data.LegXpost
+      );
     default:
-    break;
+      break;
   }
-}
+};
