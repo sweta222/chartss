01340f44fc79a22fc93a213728506506eff2facc
sweta222 <swetastuti2@gmail.com>
2020-03-26 12:59:23 +0530

    renamed file from axescreation.js to linearscale.js by separating the linear scale,band scale formation

---
+++ src/bandscale.js
@@ -0,0 +1,31 @@
+const drawBandAxis = arg => {
+  const scale = d3
+    .scaleBand()
+    .domain(arg.domainArr)
+    .range([arg.rangeMin, arg.rangeMax])
+    .paddingInner(arg.paddingI)
+    .paddingOuter(arg.paddingO);
+
+  const group = arg.parentGroup.append('g');
+
+  if (arg.type === 'column' || arg.type === 'line') {
+    const axis = d3.axisBottom(scale);
+    group
+      .attr('transform', `translate(0, ${arg.height})`)
+      .call(axis)
+      .selectAll('text')
+      .attr('transform', arg.transRotation)
+      .attr('text-anchor', arg.textAnchorpost)
+      .style('font-size', arg.xaxisTextsize);
+  } else {
+    const axis = d3.axisLeft(scale);
+    group
+      .call(axis)
+      .selectAll('text')
+      .attr('transform', arg.transRotation)
+      .attr('text-anchor', arg.textAnchorpost)
+      .style('font-size', arg.yaxisTextsize);
+  }
+
+  return scale;
+};
---
+++ src/linearscale.js
@@ -0,0 +1,26 @@
+const drawLinearAxis = arg => {
+  const scale = d3
+    .scaleLinear()
+    .domain([arg.domainMin, arg.domainMax])
+    .range([arg.rangeMin, arg.rangeMax]);
+
+  const group = arg.parentGroup.append('g');
+
+  if (arg.type === 'column' || arg.type === 'line') {
+    const axis = d3.axisLeft(scale);
+    group
+      .call(axis)
+      .selectAll('text')
+      .style('font-size', arg.yaxisTextsize);
+  } else {
+    const axis = d3.axisBottom(scale);
+    //invert x-axis to bottom
+    group
+      .attr('transform', `translate(0, ${arg.height})`)
+      .call(axis)
+      .selectAll('text')
+      .style('font-size', arg.xaxisTextsize);
+  }
+
+  return scale;
+};
