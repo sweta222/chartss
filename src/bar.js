49b21195ad5290ea50d09fac5245eec5c4899d58
sweta222 <swetastuti2@gmail.com>
2020-03-27 09:03:46 +0530

    removed and modified some parametres along with addition of required scales for drawing charts

--- src/bar.js
+++ src/bar.js
@@ -1,23 +1,48 @@
 const drawBar = arg => {
-  const bars = d3.scaleOrdinal().range(arg.bars);
+  const fillBarColor = (d, i) => arg.userData.barColor[i];
+  const xScale = drawLinearAxis({
+    domainMin: 0,
+    domainMax: d3.max(arg.userData.datasets, d => d.labely),
+    rangeMin: 0,
+    rangeMax: arg.width,
+    parentGroup: arg.parentGroup,
+    yaxisTextsize: arg.userData.yTextsize,
+    type: arg.userData.type,
+    height: arg.height,
+    xaxisTextsize: arg.userData.xTextsize
+  });
+  const yScale = drawBandAxis({
+    domainArr: arg.userData.datasets.map(d => d.labelx),
+    rangeMin: arg.height,
+    rangeMax: 0,
+    parentGroup: arg.parentGroup,
+    height: arg.height,
+    xaxisTextsize: arg.userData.xTextsize,
+    transRotation: arg.userData.xTransform,
+    textAnchorpost: arg.userData.xTextAnchor,
+    paddingI: arg.userData.innerPadding,
+    paddingO: arg.userData.outerPadding,
+    type: arg.userData.type,
+    yaxisTextsize: arg.userData.yTextsize
+  });
 
   arg.parentGroup
     .selectAll('bar')
-    .data(arg.data)
+    .data(arg.userData.datasets)
     .enter()
     .append('rect')
-    .attr('fill', arg.colorbar)
-    .attr('x', arg.barDistance)
-    .attr('height', arg.yScale3.bandwidth)
-    .attr('y', d => arg.yScale3(d[arg.AttrName]))
-    .attr('width', d => arg.xScale3(d[arg.AttrName2]));
+    .attr('fill', fillBarColor)
+    .attr('x', arg.userData.barDist)
+    .attr('height', yScale.bandwidth)
+    .attr('y', d => yScale(d[arg.AttrName]))
+    .attr('width', d => xScale(d[arg.AttrName2]));
 
   arg.svg
     .append('g')
     .append('text')
-    .text(arg.heading)
-    .style('font-size', arg.headingsize)
-    .attr('x', arg.headingXC)
-    .attr('y', arg.headingYC)
-    .style('fill', arg.headingcolor);
+    .text(arg.userData.Heading)
+    .style('font-size', arg.userData.HeadingSize)
+    .attr('x', arg.userData.HeadingStyleXval)
+    .attr('y', arg.userData.HeadingStyleYval)
+    .style('fill', arg.userData.HeadingColor);
 };
