18b53700e9a56850c98a5dc0f04b85237840317a
sweta222 <swetastuti2@gmail.com>
2020-04-02 20:19:20 +0530

    experimented

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,14 @@
 import eslint from 'rollup-plugin-eslint';
-// const examples = 'examples';
-// export default {
-//   input: 'src/main.js',
-//   output: [
-//     {
-//       file: `${examples}/bundle.cjs.js`,
-//       format: 'cjs'
-//     },
-//     {
-//       file: `${examples}/bundle.esm.js`,
-//       format: 'esm'
-//     },
-//     {
-//       name: 'spinner',
-//       file: `${examples}/bundle.umd.js`,
-//       format: 'umd'
-//     }
-//   ]
-// };
-
+import resolve from 'rollup-plugin-node-resolve';
+//import externalGlobals from 'rollup-plugin-external-globals';
+//externalGlobals({ d3: 'd3' });
+const examples = 'examples';
 export default {
-  external: ['d3'],
-  //globals: { d3: 'd3' },
   input: 'src/main.js',
   output: {
-    file: 'examples/bundle.js',
-    format: 'cjs'
+    name: 'myBundle',
+    file: `${examples}/bundle.umd.js`,
+    format: 'umd'
   },
-  plugins: [eslint]
+  plugins: [eslint, resolve()]
 };
