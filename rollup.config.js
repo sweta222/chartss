b66900c9366acd9d25a6c4762259ade1ade8cf02
sweta222 <swetastuti2@gmail.com>
2020-04-02 22:06:25 +0530

    no changes in this config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import eslint from 'rollup-plugin-eslint';
 import resolve from 'rollup-plugin-node-resolve';
-//import externalGlobals from 'rollup-plugin-external-globals';
-//externalGlobals({ d3: 'd3' });
+// import externalGlobals from 'rollup-plugin-external-globals';
+// externalGlobals({ d3: 'd3' });
 const examples = 'examples';
 export default {
   input: 'src/main.js',
