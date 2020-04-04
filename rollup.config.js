d993191dbc551a177fe070ea9a05819acd2a1ab6
sweta222 <swetastuti2@gmail.com>
2020-04-04 13:36:57 +0530

    added a plugin for making another minified version of my main file

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,23 @@
 import eslint from 'rollup-plugin-eslint';
 import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 // import externalGlobals from 'rollup-plugin-external-globals';
 // externalGlobals({ d3: 'd3' });
 const examples = 'examples';
 export default {
   input: 'src/main.js',
-  output: {
-    name: 'myBundle',
-    file: `${examples}/bundle.umd.js`,
-    format: 'umd'
-  },
-  plugins: [eslint, resolve()]
+  output: [
+    {
+      name: 'myBundle',
+      file: `${examples}/charts.umd.js`,
+      format: 'umd',
+    },
+    {
+      name: 'myBundle',
+      file: `${examples}/charts.umd.min.js`,
+      format: 'umd',
+      plugins: [terser()],
+    },
+  ],
+  plugins: [eslint(), resolve()],
 };
