11a646342ed4862c21b2e0c97982faaefccf089f
sweta222 <swetastuti2@gmail.com>
2020-04-04 16:14:00 +0530

    changed created bundled file's name along with the name field(for min version)

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,22 @@
 import eslint from 'rollup-plugin-eslint';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-// import externalGlobals from 'rollup-plugin-external-globals';
-// externalGlobals({ d3: 'd3' });
-const examples = 'examples';
+
+const build = 'build';
 export default {
   input: 'src/main.js',
   output: [
     {
       name: 'myBundle',
-      file: `${examples}/charts.umd.js`,
+      file: `${build}/charts.js`,
       format: 'umd',
     },
     {
-      name: 'myBundle',
-      file: `${examples}/charts.umd.min.js`,
+      name: 'bundleMin',
+      file: `${build}/charts.min.js`,
       format: 'umd',
       plugins: [terser()],
     },
   ],
-  plugins: [eslint(), resolve()],
+  plugins: [eslint, resolve()],
 };
